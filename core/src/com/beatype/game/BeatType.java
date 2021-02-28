41dec189edc5a2ea08127a71a195cdc6a31411ca
Jing <trashyiiiiiii@gmail.com>
2021-02-28 01:00:42 -0500

    Worked on resultScreen.

--- core/src/com/beatype/game/BeatType.java
+++ core/src/com/beatype/game/BeatType.java
@@ -3,17 +3,18 @@ package com.beatype.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.beatype.game.screens.game_screen.GameplayScreen;
 import com.beatype.game.screens.main_menu.MainMenuScreen;
 
 public class BeatType extends Game {
-	SpriteBatch batch;
-	BitmapFont font;
+	public SpriteBatch batch;
+	public BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
-		this.setScreen(new MainMenuScreen(this));
+		this.setScreen(new GameplayScreen(this));
 	}
 
 	@Override
--- core/src/com/beatype/game/screens/result_screen/ResultScreen.java
+++ core/src/com/beatype/game/screens/result_screen/ResultScreen.java
@@ -1,20 +1,31 @@
 package com.beatype.game.screens.result_screen;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.beatype.game.BeatType;
 
 public class ResultScreen implements Screen {
 
+    TextureAtlas ui;
+    TextureAtlas judge;
+    TextureAtlas numbers;
     final BeatType game;
 
-    public ResultScreen(final BeatType game) {
+    public ResultScreen(final BeatType game, int perfect, int good, int bad, int miss, double acc, int score) {
         this.game = game;
+        ui = new TextureAtlas("core/assets/result/Result.atlas");
+        judge = new TextureAtlas("core/assets/gameplay/judgement/judgements.atlas");
+        numbers = new TextureAtlas("");
 
     }
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(0,0,0,1);
+        game.batch.begin();
 
+        game.batch.end();
     }
 
     @Override
@@ -39,5 +50,7 @@ public class ResultScreen implements Screen {
 
     @Override
     public void dispose() {
+        ui.dispose();
+        judge.dispose();
     }
 }
